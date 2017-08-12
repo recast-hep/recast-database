6f319733ee93172e8d865b93fafa3830d2656a7f
lukasheinrich <lukas.heinrich@gmail.com>
2017-08-12 19:18:58 +0200

    Bump version: 0.0.1 → 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'recast-database',
-  version = '0.0.1',
+  version = '0.1.0',
   description = 'database code for RECAST',
   author = 'Lukas Heinrich',
   packages = find_packages(),
